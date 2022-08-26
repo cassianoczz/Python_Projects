2b0995589cc0931baa60d84d0adb45a6a081b640
cassianoczz <cassianoczz@users.noreply.github.com>
2022-08-26 06:15:01 -0300

    Criado a tabela Categoria para Anuncios

--- Django_Anuncios/django_anuncios_proj/anuncios_app/views.py
+++ Django_Anuncios/django_anuncios_proj/anuncios_app/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(response):
-    #todo fazer pagina home
-    return HttpResponse('Ketlin, qr namorar comigo? (anel)')
+    return render(response, 'home.html')
