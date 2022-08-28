17f1e70672d5076b19af3bf1fe2cf3a2d85ba61e
cassianoczz <cassianoczz@users.noreply.github.com>
2022-08-28 18:21:57 -0300

    Criado a metodo Desenhar para as classes

--- Python_Snake_Game/snake.py
+++ Python_Snake_Game/snake.py
@@ -9,7 +9,7 @@ RESOLUCAO = (640, 360)
 tela = pygame.display.set_mode(RESOLUCAO)
 
 
-class Fruta():
+class Fruta:
     cor = (255, 0, 0)
     tamanho = (10, 10)
 
@@ -21,16 +21,22 @@ class Fruta():
         posicao_y = random.randint(0, 35) * 10
         self.posicao = (posicao_x, posicao_y)
 
+    def desenhar(self, tela_jogo):
+        tela_jogo.blit(self.textura, self.posicao)
 
-class Snake():
+
+class Snake:
     cor = (255, 255, 255)
     tamanho = (10, 10)
 
     def __init__(self):
         self.textura = pygame.Surface(self.tamanho)
         self.textura.fill(self.cor)
-        self.posicao = [(100, 100), (90, 100)]
+        self.corpo = [(100, 100), (90, 100)]
 
+    def desenhar(self, tela_jogo):
+        for posicao in self.corpo:
+            tela_jogo.blit(self.textura, posicao)
 
 fruta = Fruta()
 cobra = Snake()
@@ -39,9 +45,8 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-    tela.blit(fruta.textura, fruta.posicao)
 
-    for posicao in cobra.posicao:
-        tela.blit(cobra.textura,posicao)
+    fruta.desenhar(tela)
+    cobra.desenhar(tela)
 
     pygame.display.update()
