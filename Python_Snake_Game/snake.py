ad8e9858b78c33f08f9186c2e1fe7c843c3cc728
cassianoczz <cassianoczz@users.noreply.github.com>
2022-08-28 18:10:14 -0300

    Criado a classe Frutinha e printado na tela do jogo

--- Python_Snake_Game/snake.py
+++ Python_Snake_Game/snake.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 pygame.init()
@@ -5,6 +7,23 @@ pygame.init()
 RESOLUCAO = (640,360)
 
 tela = pygame.display.set_mode(RESOLUCAO)
+
+class Frutinha():
+    cor = (255,0,0)
+    tamanho = (10,10)
+
+
+    def __init__(self):
+        self.textura = pygame.Surface(self.tamanho)
+        self.textura.fill(self.cor)
+
+        posicao_x = random.randint(0, 63) * 10
+        posicao_y = random.randint(0, 35) * 10
+        self.posicao = (posicao_x, posicao_y)
+
+
+frutinha = Frutinha()
+
 
 
 
@@ -14,5 +33,5 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-
+    tela.blit(frutinha.textura, frutinha.posicao)
     pygame.display.update()
