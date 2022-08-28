5aa8e4d035db190928cff10ead250711755233bb
cassianoczz <cassianoczz@users.noreply.github.com>
2022-08-28 18:15:02 -0300

    Criado a classe Cobra e printado na tela do jogo

--- Python_Snake_Game/snake.py
+++ Python_Snake_Game/snake.py
@@ -4,14 +4,14 @@ import pygame
 
 pygame.init()
 
-RESOLUCAO = (640,360)
+RESOLUCAO = (640, 360)
 
 tela = pygame.display.set_mode(RESOLUCAO)
 
-class Frutinha():
-    cor = (255,0,0)
-    tamanho = (10,10)
 
+class Fruta():
+    cor = (255, 0, 0)
+    tamanho = (10, 10)
 
     def __init__(self):
         self.textura = pygame.Surface(self.tamanho)
@@ -22,16 +22,26 @@ class Frutinha():
         self.posicao = (posicao_x, posicao_y)
 
 
-frutinha = Frutinha()
+class Snake():
+    cor = (255, 255, 255)
+    tamanho = (10, 10)
+
+    def __init__(self):
+        self.textura = pygame.Surface(self.tamanho)
+        self.textura.fill(self.cor)
+        self.posicao = [(100, 100), (90, 100)]
 
 
-
-
-
+fruta = Fruta()
+cobra = Snake()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-    tela.blit(frutinha.textura, frutinha.posicao)
+    tela.blit(fruta.textura, fruta.posicao)
+
+    for posicao in cobra.posicao:
+        tela.blit(cobra.textura,posicao)
+
     pygame.display.update()
