f5e8355c24997ad845350df0466f25adf8bfc2a5
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-13 10:10:50 -0300

    Criado funçao de criar e deletar eventos

--- Flask_API/app.py
+++ Flask_API/app.py
@@ -1,33 +1,67 @@
 from evento import Evento
 from evento_online import EventoOnline
 
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, json, request
 
 evento_online = EventoOnline("Live de Python")
 evento_online2 = EventoOnline("Live de JavaScript")
 evento = Evento("Aula de Python", "Rio de Janeiro")
-eventos = [evento_online,evento_online2, evento]
+db_eventos = [evento_online,evento_online2, evento]
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "teste"
+    return "Flask Funcionando"
+
+@app.errorhandler(400)
+def error400(erro):
+    return jsonify(erro=str(erro)), 400
 
 @app.errorhandler(404)
 def error404(erro):
     return jsonify(erro=str(erro)), 404
 
+@app.route("/api/eventos/", methods=['POST'])
+def cria_eventos():
+    dados_request = json.loads(request.data)
+    nome = dados_request.get("nome")
+    local = dados_request.get("local")
+    if not nome:
+        abort(400, "'nome' precisa ser informado")
+    if local:
+        evento = Evento(nome, local)
+    else:
+        evento = EventoOnline(nome)
+
+    db_eventos.append(evento)
+    return {
+        "id": evento.id,
+        "url": f"/api/eventos/{evento.id}" 
+    }
+
 @app.route("/api/eventos/")
-def api_eventos():
+def ler_eventos():
     lista_api_eventos = []
-    for evento in eventos:
+    for evento in db_eventos:
         lista_api_eventos.append(evento.__dict__)
     return jsonify(lista_api_eventos)
 
-@app.route("/api/eventos/<int:id>/")
-def api_eventos_id(id):
-    for evento in eventos:
+def pegar_eventos_ou_404(id):
+    for evento in db_eventos:
         if evento.id == id:
-            return jsonify(evento.__dict__)
+            return evento
     abort(404, "Evento nao encontrado")
+
+@app.route("/api/eventos/<int:id>/")
+def ler_eventos_id(id):
+    evento = pegar_eventos_ou_404(id)
+    return jsonify(evento.__dict__)
+
+@app.route("/api/eventos/<int:id>/", methods=['DELETE'])
+def deletar_eventos_id(id):
+    evento = pegar_eventos_ou_404(id)
+    db_eventos.remove(evento)
+    return jsonify(id=id)
+
+    
