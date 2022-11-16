5fa52b59c7a30629890ecb031bc23d99e4c5b2b5
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-16 06:38:16 -0300

    Refatodo as Classes para utilizar ORM Django com DB sqlite3 e feita as migrações para o DB atraves dos comandos do Django

--- Django_Site_Eventos/agenda_app/models.py
+++ Django_Site_Eventos/agenda_app/models.py
@@ -2,15 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Evento:
-    def __init__(self, nome, categoria, local=None, link=None):
-        self.nome = nome
-        self.categoria = categoria
-        self.local = local
-        self.link = link
+class Categoria(models.Model):
+    nome = models.CharField(max_length=256, unique=True)
 
+    def __str__(self):
+        return f"{self.nome} <{self.id}>"
 
-aula_python = Evento("Aula de Python", "BackEnd", "São Paulo")
-aula_js = Evento("Aula de JavaScript", "FullStack", link="https://eventos.com/")
+class Evento(models.Model):
+    nome = models.CharField(max_length=256)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    local = models.CharField(max_length=256, blank=True)
+    link = models.CharField(max_length=256, blank=True)
 
-lista_eventos = [aula_python, aula_js]
+    def __str__(self):
+        return f"{self.nome} <{self.id}>"
