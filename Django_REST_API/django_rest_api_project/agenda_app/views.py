4ff50046e6e9217ec57f4343b06bfe7514a72238
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-20 09:01:48 -0300

    Criado as views para listar os eventos de forma individual e coletiva

--- Django_REST_API/django_rest_api_project/agenda_app/views.py
+++ Django_REST_API/django_rest_api_project/agenda_app/views.py
@@ -1,7 +1,19 @@
 from agenda_app.models import Agendamento
+from agenda_app.serializers import AgendamentoSerializer
 
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
+@api_view(http_method_names=["GET"])
+def agendamento_list(resquest):
+    agendamento_qs = Agendamento.objects.all()
+    lista_agendamento = AgendamentoSerializer(agendamento_qs, many=True)
+    return JsonResponse({'data': lista_agendamento.data})
+
+@api_view(http_method_names=["GET"])
 def agendamento_details(request, id):
-    agendamentos = get_object_or_404(Agendamento, id=id)
+    agendamento_id = get_object_or_404(Agendamento, id=id)
+    agendamento_serializado = AgendamentoSerializer(agendamento_id)
+    return JsonResponse(agendamento_serializado.data)
 
