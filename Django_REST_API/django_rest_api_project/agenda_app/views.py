6a789a6d066355e8d0c67f4962d4a955aebe8b50
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-20 10:14:14 -0300

    Criado o Methodo POST para agendamentos

--- Django_REST_API/django_rest_api_project/agenda_app/views.py
+++ Django_REST_API/django_rest_api_project/agenda_app/views.py
@@ -5,11 +5,24 @@ from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def agendamento_list(resquest):
-    agendamento_qs = Agendamento.objects.all()
-    lista_agendamento = AgendamentoSerializer(agendamento_qs, many=True)
-    return JsonResponse({'data': lista_agendamento.data})
+    if resquest.method == "GET":
+        agendamento_qs = Agendamento.objects.all()
+        lista_agendamento = AgendamentoSerializer(agendamento_qs, many=True)
+        return JsonResponse({'data': lista_agendamento.data})
+    if resquest.method == "POST":
+        dados_serializado = AgendamentoSerializer(data=resquest.data)
+        if dados_serializado.is_valid():
+            Agendamento.objects.create(
+                nome_cliente = dados_serializado.validated_data["nome_cliente"],
+                data_horario_agendamento = dados_serializado.validated_data["data_horario_agendamento"],
+                telefone_cliente = dados_serializado.validated_data["telefone_cliente"],
+                email_cliente = dados_serializado.validated_data["email_cliente"],
+            )
+            return JsonResponse(dados_serializado.data, status=201)
+        return JsonResponse(dados_serializado.errors, status=400)
+
 
 @api_view(http_method_names=["GET"])
 def agendamento_details(request, id):
