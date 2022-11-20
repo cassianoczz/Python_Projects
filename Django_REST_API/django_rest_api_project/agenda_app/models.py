10a71fc078d971e0de7ad54b997968f941fc2ab4
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-20 08:57:29 -0300

    Ajustado o nome dos campos no DB e configurado a forma de apresentar os nomes na classe Agendamento

--- Django_REST_API/django_rest_api_project/agenda_app/models.py
+++ Django_REST_API/django_rest_api_project/agenda_app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Agendamento(models.Model):
-    nome = models.CharField(max_length=256)
-    data_horario = models.DateField()
-    telefone = models.CharField(max_length=14)
-    email = models.EmailField()
+    nome_cliente = models.CharField(max_length=256)
+    data_horario_agendamento = models.DateField()
+    telefone_cliente = models.CharField(max_length=14)
+    email_cliente = models.EmailField()
+
+    def __str__(self):
+        return f'<{self.id}> {self.nome_cliente}'
+
+    class Meta:
+        ordering = ['id']
