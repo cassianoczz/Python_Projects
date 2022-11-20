355c23d9bf7de5732447b26a3363b4a36118d788
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-20 09:02:04 -0300

    Configurada as URLS para as views

--- Django_REST_API/django_rest_api_project/agenda_app/urls.py
+++ Django_REST_API/django_rest_api_project/agenda_app/urls.py
@@ -1,7 +1,8 @@
-from views import agendamento_details
+from agenda_app.views import agendamento_details, agendamento_list
 
 from django.urls import path
 
 urlpatterns = [
+    path('agendamentos/', agendamento_list),
     path('agendamentos/<int:id>', agendamento_details),
 ]
