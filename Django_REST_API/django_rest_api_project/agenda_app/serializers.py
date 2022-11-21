5cc07afa9f8ff4038690165191fe4e01f33f7b64
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-21 07:55:24 -0300

    Refatorado a logica de POST e PATCH, migrado para dentro da classe serializadora

--- Django_REST_API/django_rest_api_project/agenda_app/serializers.py
+++ Django_REST_API/django_rest_api_project/agenda_app/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
+from django.utils import timezone
+
+from agenda_app.models import Agendamento
 
 class AgendamentoSerializer(serializers.Serializer):
     nome_cliente = serializers.CharField(max_length=256)
     data_horario_agendamento = serializers.DateTimeField()
     telefone_cliente = serializers.CharField(max_length=14)
     email_cliente = serializers.EmailField()
+            
+
+    def create(self, validated_data):
+        agendamento = Agendamento.objects.create(
+            nome_cliente = validated_data["nome_cliente"],
+            data_horario_agendamento = validated_data["data_horario_agendamento"],
+            telefone_cliente = validated_data["telefone_cliente"],
+            email_cliente = validated_data["email_cliente"],
+            )
+        return agendamento
+
+    def update(self, instance, validated_data):
+        instance.nome_cliente = validated_data.get("nome_cliente", instance.nome_cliente)
+        instance.data_horario_agendamento = validated_data.get("data_horario_agendamento", instance.data_horario_agendamento)
+        instance.telefone_cliente = validated_data.get("telefone_cliente", instance.telefone_cliente)
+        instance.email_cliente = validated_data.get("email_cliente", instance.email_cliente)
+        instance.save()
+        return instance
--- Django_REST_API/django_rest_api_project/agenda_app/views.py
+++ Django_REST_API/django_rest_api_project/agenda_app/views.py
@@ -12,15 +12,11 @@ def agendamento_list(resquest):
         agendamento_qs = Agendamento.objects.all()
         lista_agendamento = AgendamentoSerializer(agendamento_qs, many=True)
         return JsonResponse({'data': lista_agendamento.data})
+
     if resquest.method == "POST":
         dados_serializado = AgendamentoSerializer(data=resquest.data)
         if dados_serializado.is_valid():
-            Agendamento.objects.create(
-                nome_cliente = dados_serializado.validated_data["nome_cliente"],
-                data_horario_agendamento = dados_serializado.validated_data["data_horario_agendamento"],
-                telefone_cliente = dados_serializado.validated_data["telefone_cliente"],
-                email_cliente = dados_serializado.validated_data["email_cliente"],
-            )
+            dados_serializado.save()
             return JsonResponse(dados_serializado.data, status=201)
         return JsonResponse(dados_serializado.errors, status=400)
 
@@ -31,17 +27,14 @@ def agendamento_details(request, id):
     if request.method == "GET":
         agendamento_serializado = AgendamentoSerializer(agendamento_id)
         return JsonResponse(agendamento_serializado.data)
+
     if request.method == "PATCH":
-        agendamento_serializado = AgendamentoSerializer(data=request.data, partial=True)
+        agendamento_serializado = AgendamentoSerializer(agendamento_id, data=request.data, partial=True)
         if agendamento_serializado.is_valid():
-            dados_valid = agendamento_serializado.validated_data
-            agendamento_id.nome_cliente = dados_valid.get("nome_cliente", agendamento_id.nome_cliente)
-            agendamento_id.data_horario_agendamento = dados_valid.get("data_horario_agendamento", agendamento_id.data_horario_agendamento)
-            agendamento_id.telefone_cliente = dados_valid.get("telefone_cliente", agendamento_id.telefone_cliente)
-            agendamento_id.email_cliente = dados_valid.get("email_cliente", agendamento_id.email_cliente)
-            agendamento_id.save()
-            return JsonResponse(dados_valid, status=200)
+            agendamento_serializado.save()
+            return JsonResponse(agendamento_serializado.data, status=200)
         return JsonResponse(agendamento_serializado.errors, status=400)
+
     if request.method == "DELETE":
         #agendamento_id.cancelando = True AO INVEZ DE DELETAR O ENVENTO PARA MANTER HISTORICO
         agendamento_id.delete()
