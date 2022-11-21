78f445f5691b12c3440cf1d6f31f0555dc8b84ce
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-21 10:08:02 -0300

    Refatorado a serializacao dos dados e excluido create e update pois agora é padrão da classe Meta.

--- Django_REST_API/django_rest_api_project/agenda_app/serializers.py
+++ Django_REST_API/django_rest_api_project/agenda_app/serializers.py
@@ -4,10 +4,10 @@ from rest_framework import serializers
 from django.utils import timezone
 
 class AgendamentoSerializer(serializers.Serializer):
-    nome_cliente = serializers.CharField(max_length=256)
-    data_horario_agendamento = serializers.DateTimeField()
-    telefone_cliente = serializers.CharField(max_length=14)
-    email_cliente = serializers.EmailField()
+
+    class Meta:
+        model = Agendamento
+        fields = ["id", "nome_cliente", "data_horario_agendamento", "telefone_cliente", "email_cliente"]
 
     def validate(self, attrs):
         telefone_cliente = attrs.get("telefone_cliente", "")
@@ -19,21 +19,3 @@ class AgendamentoSerializer(serializers.Serializer):
         if value < timezone.now():
             raise serializers.ValidationError("Data deve ser maior ou igual a data atual.")
         return value
-            
-
-    def create(self, validated_data):
-        agendamento = Agendamento.objects.create(
-            nome_cliente = validated_data["nome_cliente"],
-            data_horario_agendamento = validated_data["data_horario_agendamento"],
-            telefone_cliente = validated_data["telefone_cliente"],
-            email_cliente = validated_data["email_cliente"],
-            )
-        return agendamento
-
-    def update(self, instance, validated_data):
-        instance.nome_cliente = validated_data.get("nome_cliente", instance.nome_cliente)
-        instance.data_horario_agendamento = validated_data.get("data_horario_agendamento", instance.data_horario_agendamento)
-        instance.telefone_cliente = validated_data.get("telefone_cliente", instance.telefone_cliente)
-        instance.email_cliente = validated_data.get("email_cliente", instance.email_cliente)
-        instance.save()
-        return instance
