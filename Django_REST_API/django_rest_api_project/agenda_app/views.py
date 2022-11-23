45f05a79c4cb0a3fabba25e3ddb87c0c13cb115d
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-23 09:19:39 -0300

    Convertido para Generic VIew

--- Django_REST_API/django_rest_api_project/agenda_app/views.py
+++ Django_REST_API/django_rest_api_project/agenda_app/views.py
@@ -1,42 +1,14 @@
 from agenda_app.models import Agendamento
 from agenda_app.serializers import AgendamentoSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class AgendamentoList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView
-):
+class AgendamentoList(generics.ListCreateAPIView):
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class AgendamentoDetails(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView
-):
+class AgendamentoDetails(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
