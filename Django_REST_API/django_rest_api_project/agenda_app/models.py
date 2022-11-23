5c1cf5f2945a32f11af670735ef101e4ee5c7074
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-23 09:37:44 -0300

    Implementado o campo prestador para vincular diversos prestadores de serviço autenticados para cadastro de novos agendamentos

--- Django_REST_API/django_rest_api_project/agenda_app/models.py
+++ Django_REST_API/django_rest_api_project/agenda_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 
 class Agendamento(models.Model):
+    prestador = models.ForeignKey(
+        'auth.User', 
+        related_name='agendamentos',
+        on_delete=models.CASCADE
+        )
+
     nome_cliente = models.CharField(max_length=256)
     data_horario_agendamento = models.DateTimeField()
     telefone_cliente = models.CharField(max_length=14)
