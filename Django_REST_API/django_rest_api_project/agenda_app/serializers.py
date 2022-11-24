f7970256790c483a4749be9eb48df1d435d1c802
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-24 07:30:44 -0300

    Criado validação para prestador

--- Django_REST_API/django_rest_api_project/agenda_app/serializers.py
+++ Django_REST_API/django_rest_api_project/agenda_app/serializers.py
@@ -2,21 +2,32 @@ from agenda_app.models import Agendamento
 
 from rest_framework import serializers
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class AgendamentoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Agendamento
-        fields = ["id", "nome_cliente", "data_horario_agendamento", "telefone_cliente", "email_cliente"]
+        fields = '__all__'
+        # fields = ["id", "nome_cliente", "data_horario_agendamento", "telefone_cliente", "email_cliente"]
+
+    prestador = serializers.CharField()
+
+    def validate_prestador(self, value):
+        try:
+            prestador = User.objects.get(username=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("prestador não existe.")
+        return prestador
+
+    def validate_data_horario_agendamento(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Data deve ser maior ou igual a data atual.")
+        return value
 
     def validate(self, attrs):
         telefone_cliente = attrs.get("telefone_cliente", "")
         if not telefone_cliente.startswith("+55"):
             raise serializers.ValidationError("Telefone deve ser brasileiro.")
         return attrs
-
-    def validate_data_horario_agendamento(self, value):
-        if value < timezone.now():
-            raise serializers.ValidationError("Data deve ser maior ou igual a data atual.")
-        return value
