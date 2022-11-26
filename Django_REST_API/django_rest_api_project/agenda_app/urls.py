c0d0307151a224eb0d953ce29fff26eef8b4f112
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-26 10:46:14 -0300

    Criado rota para mostrar agendamentos por Prestado

--- Django_REST_API/django_rest_api_project/agenda_app/urls.py
+++ Django_REST_API/django_rest_api_project/agenda_app/urls.py
@@ -1,8 +1,9 @@
-from agenda_app.views import AgendamentoDetails, AgendamentoList
+from agenda_app.views import PrestadorList, AgendamentoDetails, AgendamentoList
 
 from django.urls import path
 
 urlpatterns = [
+    path('prestadores/', PrestadorList.as_view()),
     path('agendamentos/', AgendamentoList.as_view()),
     path('agendamentos/<int:pk>', AgendamentoDetails.as_view()),
 ]
