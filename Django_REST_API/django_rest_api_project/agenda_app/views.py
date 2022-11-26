3bfa16929bd2cab9889a0acd132b2c6bf276e2a0
cassianoczz <cassianoczz@users.noreply.github.com>
2022-11-26 10:26:11 -0300

    Permissionado visualizaçao/criação de agendamento

--- Django_REST_API/django_rest_api_project/agenda_app/views.py
+++ Django_REST_API/django_rest_api_project/agenda_app/views.py
@@ -1,10 +1,29 @@
 from agenda_app.models import Agendamento
 from agenda_app.serializers import AgendamentoSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
+
+
+class IsOwnerOrCreateOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        prestador = request.query_params.get('prestador', None)
+        if request.method == 'POST':
+            return True
+        if request.user.username == prestador:
+            return True
+        return False
+
+
+class IsPrestador(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if obj.prestador == request.user:
+            return True
+        return False
 
 
 class AgendamentoList(generics.ListCreateAPIView):
-   
+
+    permission_classes = [IsOwnerOrCreateOnly]
     serializer_class = AgendamentoSerializer
 
     def get_queryset(self):
@@ -15,5 +34,6 @@ class AgendamentoList(generics.ListCreateAPIView):
 
 class AgendamentoDetails(generics.RetrieveUpdateDestroyAPIView):
 
-    queryset = Agendamento.objects.all()
+    permission_classes = [IsPrestador]
     serializer_class = AgendamentoSerializer
+    queryset = Agendamento.objects.all()
